a61fb0f7a10390fe82f8779be243fa2940dcb17c
GtDavide01 <davidefiorini01@gmail.com>
2022-10-28 15:32:26 +0200

    esercizio concluso

--- js/script.js
+++ js/script.js
@@ -1,19 +1,28 @@
 // Descrizione:
 // Attraverso l'apposita API di Boolean
 // https://flynn.boolean.careers/exercises/api/random/mail
-// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 // Bonus
 // Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 
+const {createApp} = Vue ; 
 
-
-
-const listEmail = [];
-for(let i = 0 ; i < 10 ; i++ ){
-    axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-.then( function(risposta){
-    console.log(risposta.data.response);
+createApp({
+    data(){
+        return{
+            listEmail: []
+        }
+    },
+    created(){
+        // generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+        for(let i = 0 ; i < 10 ; i++ ){
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then( (risposta) => {
+            console.log(risposta.data.response);
+            this.listEmail.push(risposta.data.response);
+            }
+        )
+        }
     }
-)
+}).mount("#app");
 
-}
+
