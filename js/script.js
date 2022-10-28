74dd8bda7b2f82d51d4644ba25235dc2d70b7d2e
GtDavide01 <davidefiorini01@gmail.com>
2022-10-28 17:33:11 +0200

    esercizio concluso con promise.all

--- js/script.js
+++ js/script.js
@@ -18,10 +18,13 @@ createApp({
             for(let i = 0 ; i < 10 ; i++ ){
                 arrayListEmail.push(axios.get("https://flynn.boolean.careers/exercises/api/random/mail"));
                 console.log(arrayListEmail);
+            }
+            for(let i = 0 ; i < 10 ; i++){
                 Promise.all(arrayListEmail).then((risultati)=>{
                     this.listEmail.push(risultati[i].data.response);
-                })
+            })
             }
+            
         },
     },
     created(){
